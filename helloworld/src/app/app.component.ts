cd261b57ff743675b726a6ad3e222ce649c9594a
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-03-07 12:18:09 +0530

    [Bhupendra Shakya] Add . Lounching Bridgelabz site while clicking on logo  .

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -9,9 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'helloworld';
   imgUrl="assets/BL_logo_square_jpg.jpg";
+  url="https://www.bridgelabz.com/";
   ngOnInit(): void{
     this.title="Hello from BridgeLabz ! ";
-   
+
+  }
+
+  onClick($event: Event){
+    console.log("Save button is clicked !", Event);
+    window.open(this.url, "_blank");
+
   }
   
 }
