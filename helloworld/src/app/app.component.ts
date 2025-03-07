1f8d60d6e08beadf20da08d8adae9536b9531ea6
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-03-07 13:19:39 +0530

    [Bhupendra Shakya] Add .display Hello from BridgeLabz with a user inputted name with validation

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -1,25 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  standalone: false,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'helloworld';
-  imgUrl="assets/BL_logo_square_jpg.jpg";
-  url="https://www.bridgelabz.com/";
-  userName: String = "";
-  ngOnInit(): void{
-    this.title="Hello from BridgeLabz ! ";
+  title = 'Helloworld';
+  imgUrl= "../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = "";
+  nameError: string = "";
 
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+    
   }
 
   onClick($event: Event){
-    console.log("Save button is clicked !", Event);
+    console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
-
   }
-  
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!", $event);
+    const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/; // Regex: First letter capital, min 3 characters
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+    } else {
+      this.nameError = "Name is Incorrect!";
+    }
+  }
+
 }
